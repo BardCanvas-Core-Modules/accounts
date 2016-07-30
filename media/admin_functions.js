aad61102250ab1fbc67988302917e3dc2ede975d
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-30 03:25:35 -0500

    Added level setter

--- media/admin_functions.js
+++ media/admin_functions.js
@@ -71,3 +71,62 @@ function toggle_account(id_account, new_mode)
         location.href = $_PHP_SELF + '?wasuuup=' + parseInt(Math.random() * 1000000000000000);
     });
 }
+
+function open_level_switcher(trigger, id_account, current_level)
+{
+    var $target = $(trigger).closest('.user_level_switcher').find('.target');
+    var $source = $(trigger).closest('.current');
+    var $select = $('#level_switch').find('select').clone();
+    // var span    = '<span class="fa fa-ban fa-fw pseudo_link" onclick="cancel_level_switching(this)"></span>';
+    
+    $select.attr('data-id-account', id_account);
+    $select.find('option[value="' + current_level + '"]').prop('selected', true);
+    $source.hide();
+    $target.append($select).show();
+}
+
+function cancel_level_switching(trigger)
+{
+    var $target  = $(trigger).closest('.target');
+    var $current = $(trigger).closest('.user_level_switcher').find('.current');
+    
+    $target.html('').hide();
+    $current.show();
+}
+
+function change_user_level(trigger)
+{
+    var $trigger   = $(trigger);
+    var value      = $trigger.find('option:selected').val();
+    var id_account = $trigger.attr('data-id-account');
+    
+    if( value == '!@cancel' )
+    {
+        cancel_level_switching(trigger);
+        
+        return;
+    }
+    
+    var url = $_FULL_ROOT_PATH + '/accounts/scripts/toolbox.php';
+    var params = {
+        mode:       'change_level',
+        id_account: id_account,
+        level:      value,
+        wasuuup:    parseInt(Math.random() * 1000000000000000)
+    };
+    
+    var $tr = $('#accounts_nav').find('tr[id_account="' + id_account + '"]');
+    $tr.block(blockUI_smallest_params);
+    $.get(url, params, function(response)
+    {
+        if( response != 'OK' )
+        {
+            alert( response );
+            $tr.unblock();
+            
+            return;
+        }
+        
+        location.href = $_PHP_SELF + '?wasuuup=' + parseInt(Math.random() * 1000000000000000);
+    });
+}
--- scripts/toolbox.php
+++ scripts/toolbox.php
@@ -6,6 +6,7 @@
  * @subpackage accounts
  * @author     Alejandro Caballero - lava.caballero@gmail.com
  *
+ * @var config            $config
  * @var account           $account
  * @var template          $template
  * @var settings          $settings
@@ -16,6 +17,7 @@
  */
 
 use hng2_base\account;
+use hng2_base\config;
 use hng2_base\settings;
 use hng2_base\template;
 
@@ -118,15 +120,51 @@ switch( $_REQUEST["mode"] )
         $user_account = new account($_REQUEST["id_account"]);
         if( ! $user_account->_exists )
             die( $current_module->language->admin->record_nav->action_messages->target_not_exists );
-    
+        
         if( $account->id_account == $user_account->id_account )
             die( $current_module->language->admin->record_nav->action_messages->no_self_enable_disable );
-    
+        
         $user_account->disable();
         send_notification(
             $account->id_account,
             "information",
             $current_module->language->admin->record_nav->action_messages->enabled_ok
+        );
+        
+        die("OK");
+        break;
+    
+    case "change_level":
+        
+        if( $account->level < $config::MODERATOR_USER_LEVEL )
+            die( $current_module->language->admin->record_nav->action_messages->level_change_denied );
+        
+        $user_account = new account($_REQUEST["id_account"]);
+        
+        if( ! $user_account->_exists )
+            die( $current_module->language->admin->record_nav->action_messages->target_not_exists );
+        
+        if( $account->id_account == $user_account->id_account )
+            die( $current_module->language->admin->record_nav->action_messages->no_self_level_change );
+        
+        if( $user_account->level > $account->level )
+            die( $current_module->language->admin->record_nav->action_messages->no_upper_level_allowed );
+        
+        if( ! is_numeric($_GET["level"]) )
+            die( $current_module->language->admin->record_nav->action_messages->invalid_level_specified );
+        
+        if( ! isset($config->user_levels_by_level[$_GET["level"]]))
+            die( $current_module->language->admin->record_nav->action_messages->invalid_level_specified );
+        
+        $user_account->set_level( $_GET["level"] );
+        send_notification(
+            $account->id_account,
+            "information",
+            replace_escaped_vars(
+                $current_module->language->admin->record_nav->action_messages->level_change_ok,
+                array('{$user}', '{$level}', '{$level_name}'),
+                array($user_account->display_name, $user_account->level, $config->user_levels_by_level[$user_account->level])
+            )
         );
         
         die("OK");
