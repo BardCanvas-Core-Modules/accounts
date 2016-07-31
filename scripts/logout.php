de8fc831a7b80626ff818ef5e3e1d1789a8e7325
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-31 08:06:34 -0500

    Fixed redirection issue

--- scripts/logout.php
+++ scripts/logout.php
@@ -5,12 +5,18 @@
  * @package    HNG2
  * @subpackage accounts
  * @author     Alejandro Caballero - lava.caballero@gmail.com
+ * 
+ * @var config  $config
+ * @var account $account
  */
+
+use hng2_base\account;
+use hng2_base\config;
 
 include "../../config.php";
 include "../../includes/bootstrap.inc";
 
 $account->close_session();
 
-$go = empty($_REQUEST["go"]) ? $config->full_root_path : $_REQUEST["go"];
-header("Location: " . $go);
+$go = empty($_REQUEST["go"]) ? "{$config->full_root_path}/" : $_REQUEST["go"];
+header("Location: {$go}");
