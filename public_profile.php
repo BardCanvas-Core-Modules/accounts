3f425e3adb6806d1cfd3907c9a5203e7e81a7e8b
Alejandro Caballero <lava.caballero@gmail.com>
2016-08-04 01:37:12 -0500

    Added support for user profile home

--- public_profile.php
+++ public_profile.php
@@ -23,14 +23,16 @@ if( empty($_GET["slug"]) ) throw_fake_404();
 $author = new account($_GET["slug"]);
 if( ! $author->_exists ) throw_fake_404();
 
-$template->set("page_tag", "post_author_index");
-$template->set("showing_archive", true);
+$template->set("page_tag", "user_home");
+//$template->set("showing_archive", true);
 $template->set("show_user_profile_heading", true);
 $template->set("user_profile_account", $author);
-$template->set("current_user_profile_tab", "posts");
-$template->page_contents_include = "by_author.inc";
+$template->set("current_user_profile_tab", "@root");
 $template->set_page_title(replace_escaped_vars(
-    $current_module->language->pages->by_author->title, '{$author}', $author->display_name
+    $current_module->language->user_profile_page->title, '{$user}', $author->display_name
 ));
-$template->append("additional_body_attributes", " data-listing-type='archive'");
+//$template->append("additional_body_attributes", " data-listing-type='archive'");
+
+# @hack: The definition below is a hack that shouldn't be repeated unless strictly neccesary :P
+$template->page_contents_include = "../../templates/{$template->name}/segments/user_profile_home.inc";
 include "{$template->abspath}/main.php";
