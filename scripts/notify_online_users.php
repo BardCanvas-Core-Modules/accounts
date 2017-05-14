7fdd6ef1805e7943ad324db6547e65c1c3c5270c
Alejandro Caballero <lava.caballero@gmail.com>
2017-05-13 20:04:04 -0500

    Added forced self inclussion

--- scripts/notify_online_users.php
+++ scripts/notify_online_users.php
@@ -30,10 +30,11 @@ if( ! in_array($type, array("information", "success", "alert", "warning", "error
     die($current_module->language->notify_online_users->messages->invalid_type);
 
 $repository = new accounts_repository();
-$users      = $repository->get_online_users_list(10, false);
+$users      = $repository->get_online_users_list(10);
 if( empty($users) ) die($current_module->language->notify_online_users->messages->none_online);
 
 $message = nl2br($message);
+send_notification( $account->id_account, $type, convert_emojis($message) );
 foreach($users as $user_data)
     send_notification( $user_data->id_account, $type, convert_emojis($message) );
 
